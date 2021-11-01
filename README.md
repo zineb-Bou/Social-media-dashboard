37f370cfd4f82bec6f279621915931bfe06fc411
unknown <boutaazineb08@gmail.com>
2021-11-01 23:56:44 +0100

    :octocat: :atom_symbol: First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Social media dashboard theme switcher using React and Sass
---
+++ src/js/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../sass/style.scss';
+
+const App = () => <>Hello Wolrd!</>;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
