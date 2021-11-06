f14523c8adcfd6b2e5bacd37b35345add89683bd
unknown <boutaazineb08@gmail.com>
2021-11-06 14:12:27 +0100

    :heavy_plus_sign:  Add the localStorage option to theme switch button.

--- src/js/components/switchThemeButton.js
+++ src/js/components/switchThemeButton.js
@@ -1,48 +1,65 @@
 import React from 'react';
-function addBodyClass(className) {
-  document.body.classList.add(className);
-}
-function removeBodyClass(className) {
-  document.body.classList.remove(className);
-}
-const colorPreferance = window.matchMedia(
-  '(prefers-color-scheme: dark)'
-).matches;
-
 class SwitchThemeButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: colorPreferance,
+      active: '',
     };
   }
-  isActive() {
-    return this.state.active;
+  colorModeFromPreferences() {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light'; // If preference is set or does not match anything (light is default)
+  }
+  colorModeFromLocalStorage() {
+    return localStorage.getItem('colorMode');
+  }
+  componentDidMount() {
+    this.loadAndUpdateColor();
+  }
+  loadAndUpdateColor() {
+    // local storage has precendence over the prefers-color-scheme
+    const color =
+      this.colorModeFromLocalStorage() || this.colorModeFromPreferences();
+    if (color == 'dark') {
+      this.setState({
+        active: 'dark',
+      });
+      document.body.classList = 'dark';
+    } else if (color == 'light') {
+      this.setState({
+        active: 'light',
+      });
+      document.body.classList = 'light';
+    }
   }
   toggle() {
-    this.setState({
-      active: !this.isActive(),
-    });
-    if (this.isActive()) {
-      addBodyClass('light');
-      removeBodyClass('dark');
-    } else {
-      addBodyClass('dark');
-      removeBodyClass('light');
+    if (this.state.active == 'dark') {
+      document.body.classList = 'light';
+      localStorage.setItem('colorMode', 'light');
+      this.setState({
+        active: 'light',
+      });
+    } else if (this.state.active == 'light') {
+      this.setState({
+        active: 'dark',
+      });
+      document.body.classList = 'dark';
+      localStorage.setItem('colorMode', 'dark');
     }
   }
   render() {
     return (
       <>
         <button
-          aria-pressed={this.isActive.bind(this)}
+          aria-pressed={this.state.active}
           className="header__toggle"
           aria-label="theme toggle"
           onClick={this.toggle.bind(this)}
         >
-          {this.isActive() ? 'Light theme' : 'Dark theme'}
+          {this.state.active == 'dark' ? 'Light theme' : 'Dark theme'}
           <span
-            className={`toggle ${this.state.active ? 'move' : ''}`}
+            className={`toggle ${this.state.active == 'dark' ? 'move' : ''} `}
             aria-hidden="true"
           ></span>
         </button>
@@ -50,5 +67,4 @@ class SwitchThemeButton extends React.Component {
     );
   }
 }
-
 export default SwitchThemeButton;
